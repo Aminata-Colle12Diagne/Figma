261651216f617a5a4cb51ff51be75fb14056804c
Aminata-Colle12Diagne <98230791+Aminata-Colle12Diagne@users.noreply.github.com>
2024-04-18 16:19:01 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const router = express.Router(); // Création d'un routeur Express
-app.use(express.json());
 const crypto = require('crypto');
 const cors = require('cors');
-const serverless = require('serverless-http'); // Importer serverless-http
+app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 10000;
@@ -13,7 +11,6 @@ require('./db/connection');
 const Users = require('./Model/User');
 const UserModal = require('./Model/Data');
 
-// Configuration des routes
 app.post("/", async (req, res) => {
     let user = new Users(req.body);
     let result = await user.save();
@@ -56,33 +53,19 @@ app.post('/forgot-password', (req, res) => {
     });
 });
 
-// Endpoint pour ajouter des données d'hôtel
 app.post("/addHotel", async (req, res) => {
-    const hotelData = req.body; // Les données d'hôtel envoyées depuis le frontend
+    const hotelData = req.body;
 
     try {
-        // Créer une nouvelle instance du modèle d'hôtel avec les données reçues
         const newHotel = new UserModal(hotelData);
-
-        // Enregistrer les données de l'hôtel dans la base de données
         await newHotel.save();
-
-        // Répondre avec les données de l'hôtel ajoutées
         res.json(newHotel);
     } catch (error) {
-        // En cas d'erreur lors de l'ajout des données de l'hôtel
         console.error(error);
         res.status(500).send("Erreur lors de l'ajout des données de l'hôtel");
     }
 });
 
-// Utilisation du routeur pour les fonctions serverless
-app.use("/.netlify/functions/app", router);
-
-// Exporter une version serverless de l'application Express
-module.exports.handler = serverless(app);
-
-// Démarrage du serveur Express
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Serveur démarré sur http://0.0.0.0:${PORT}`);
 });
