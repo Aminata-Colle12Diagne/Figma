c5379250fe7335e979a44842812195cc719caa2c
Aminata-Colle12Diagne <diagneaminata464@gmail.com>
2024-04-22 09:11:50 +0000

    updated

--- client/src/Composants/auth/Login.jsx
+++ client/src/Composants/auth/Login.jsx
@@ -11,7 +11,7 @@ function Login(props) {
     const collectionData = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:10000', {
+            const response = await fetch('http://localhost:10000/', {
                 method: 'POST',
                 body: JSON.stringify({email, password }),
                 headers: {
